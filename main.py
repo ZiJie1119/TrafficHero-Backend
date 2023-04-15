50cc8d1b271bf618dec7d06f9fb97574ccef4caf
Eddie <zxc1435693@gmail.com>
2023-04-15 12:20:04 +0800

    Merge "Verify the TDX Account"

--- main.py
+++ main.py
@@ -23,39 +23,39 @@ app = FastAPI(openapi_tags=tags_metadata)
 app_id = 'b10923015-1aa2500a-b917-4539'
 app_key = '009cb4d7-a507-47f1-bab6-7da5182e6e95'
 auth_url = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token"
-url = "https://tdx.transportdata.tw/api/basic/v1/Parking/OffStreet/ParkingAvailability/Road/Freeway/ServiceArea?%24top=30&%24format=JSON"
+class Auth():
+    def __init__(self, app_id, app_key):
+        self.app_id = app_id
+        self.app_key = app_key
+
+    def get_auth_header(self):
+        content_type = 'application/x-www-form-urlencoded'
+        grant_type = 'client_credentials'
+
+        return {
+            'content-type': content_type,
+            'grant_type': grant_type,
+            'client_id': self.app_id,
+            'client_secret': self.app_key
+        }
+class data():
+    def __init__(self, app_id, app_key, auth_response):
+        self.app_id = app_id
+        self.app_key = app_key
+        self.auth_response = auth_response
+    def get_data_header(self):
+        auth_JSON = json.loads(self.auth_response.text)
+        access_token = auth_JSON.get('access_token')
+        return {
+            'authorization': 'Bearer '+access_token
+    }
 # ----------Verify the TDX Account----------#
 
 @app.get("/serviceArea",tags=["serviceArea"])
 async def serviceArea():
+    url = "https://tdx.transportdata.tw/api/basic/v1/Parking/OffStreet/ParkingAvailability/Road/Freeway/ServiceArea?%24top=30&%24format=JSON"
     serviceAreaName = []
     serviceAreaSpace = []
-    class Auth():
-        def __init__(self, app_id, app_key):
-            self.app_id = app_id
-            self.app_key = app_key
-
-        def get_auth_header(self):
-            content_type = 'application/x-www-form-urlencoded'
-            grant_type = 'client_credentials'
-
-            return {
-                'content-type': content_type,
-                'grant_type': grant_type,
-                'client_id': self.app_id,
-                'client_secret': self.app_key
-            }
-    class data():
-        def __init__(self, app_id, app_key, auth_response):
-            self.app_id = app_id
-            self.app_key = app_key
-            self.auth_response = auth_response
-        def get_data_header(self):
-            auth_JSON = json.loads(self.auth_response.text)
-            access_token = auth_JSON.get('access_token')
-            return {
-                'authorization': 'Bearer '+access_token
-        }
     try:
         d = data(app_id, app_key, auth_response)
         data_response = requests.get(url, headers=d.get_data_header())
@@ -73,33 +73,8 @@ async def serviceArea():
 
 @app.get("/cityParking/{cityName}",tags = ["cityParking"])
 async def cityParking(cityName):
+    url = "https://tdx.transportdata.tw/api/basic/v1/Parking/OffStreet/ParkingAvailability/City/"+cityName+"?%24top=30&%24format=JSON"
     cityParkingSpace = []
-    class Auth():
-        def __init__(self, app_id, app_key):
-            self.app_id = app_id
-            self.app_key = app_key
-
-        def get_auth_header(self):
-            content_type = 'application/x-www-form-urlencoded'
-            grant_type = 'client_credentials'
-
-            return {
-                'content-type': content_type,
-                'grant_type': grant_type,
-                'client_id': self.app_id,
-                'client_secret': self.app_key
-            }
-    class data():
-        def __init__(self, app_id, app_key, auth_response):
-            self.app_id = app_id
-            self.app_key = app_key
-            self.auth_response = auth_response
-        def get_data_header(self):
-            auth_JSON = json.loads(self.auth_response.text)
-            access_token = auth_JSON.get('access_token')
-            return {
-                'authorization': 'Bearer '+access_token
-        }
     try:
         d = data(app_id, app_key, auth_response)
         data_response = requests.get(url, headers=d.get_data_header())
@@ -116,33 +91,8 @@ async def cityParking(cityName):
 
 @app.get("/sideParking/{cityName}",tags = ["sideParking"])
 async def sideParking(cityName):
+    url = "https://tdx.transportdata.tw/api/basic/v1/Parking/OnStreet/ParkingSegmentAvailability/City/"+cityName+"?%24top=30&%24format=JSON"
     sideParkingSpace = []
-    class Auth():
-        def __init__(self, app_id, app_key):
-            self.app_id = app_id
-            self.app_key = app_key
-
-        def get_auth_header(self):
-            content_type = 'application/x-www-form-urlencoded'
-            grant_type = 'client_credentials'
-
-            return {
-                'content-type': content_type,
-                'grant_type': grant_type,
-                'client_id': self.app_id,
-                'client_secret': self.app_key
-            }
-    class data():
-        def __init__(self, app_id, app_key, auth_response):
-            self.app_id = app_id
-            self.app_key = app_key
-            self.auth_response = auth_response
-        def get_data_header(self):
-            auth_JSON = json.loads(self.auth_response.text)
-            access_token = auth_JSON.get('access_token')
-            return {
-                'authorization': 'Bearer '+access_token
-        }
     try:
         d = data(app_id, app_key, auth_response)
         data_response = requests.get(url, headers=d.get_data_header())
@@ -156,4 +106,3 @@ async def sideParking(cityName):
     for side in dataAll["CurbParkingSegmentAvailabilities"]:
         sideParkingSpace.append(side["ParkingSegmentName"]["Zh_tw"] +" 剩餘位置： " +str(side["AvailableSpaces"]))
     return {"sideParking":sideParkingSpace}
-
