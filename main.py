4d5c24ec3ced708c912a5cbe5c8b537d39ef2ff1
Eddie <zxc1435693@gmail.com>
2023-04-26 16:23:25 +0800

    refactor: add auth.py to reduce main.py lengthy

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from pprint import pprint
 import json
 from fastapi import FastAPI
+from auth import get_data_response
 
 tags_metadata = [
     {
@@ -19,57 +20,15 @@ tags_metadata = [
 ]
 app = FastAPI(openapi_tags=tags_metadata)
 
-# ----------Verify the TDX Account----------#
 app_id = 'b10923015-1aa2500a-b917-4539'
 app_key = '009cb4d7-a507-47f1-bab6-7da5182e6e95'
-auth_url = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token"
-class Auth():
-    def __init__(self, app_id, app_key):
-        self.app_id = app_id
-        self.app_key = app_key
-
-    def get_auth_header(self):
-        content_type = 'application/x-www-form-urlencoded'
-        grant_type = 'client_credentials'
-
-        return {
-            'content-type': content_type,
-            'grant_type': grant_type,
-            'client_id': self.app_id,
-            'client_secret': self.app_key
-        }
-class data():
-    def __init__(self, app_id, app_key, auth_response):
-        self.app_id = app_id
-        self.app_key = app_key
-        self.auth_response = auth_response
-    def get_data_header(self):
-        auth_JSON = json.loads(self.auth_response.text)
-        access_token = auth_JSON.get('access_token')
-        return {
-            'authorization': 'Bearer '+access_token
-    }
-
-def response(url):
-    data_response = ""
-    try:
-        d = data(app_id, app_key, auth_response)
-        data_response = requests.get(url, headers=d.get_data_header())
-    except:
-        a = Auth(app_id, app_key)
-        auth_response = requests.post(auth_url, a.get_auth_header())
-        d = data(app_id, app_key, auth_response)
-        data_response = requests.get(url, headers=d.get_data_header())
-    dataAll = json.loads(data_response.text)
-    return dataAll
-# ----------Verify the TDX Account----------#
 
 @app.get("/serviceArea",tags=["serviceArea"])
 async def serviceArea():
     url = "https://tdx.transportdata.tw/api/basic/v1/Parking/OffStreet/ParkingAvailability/Road/Freeway/ServiceArea?%24top=30&%24format=JSON"
     serviceAreaName = []
     serviceAreaSpace = []
-    dataAll = response(url)
+    dataAll = get_data_response(app_id, app_key, url)
     for service in dataAll["ParkingAvailabilities"]:
         # serviceAreaName.append(service["CarParkName"]["Zh_tw"])
         serviceAreaSpace.append(service["CarParkName"]["Zh_tw"]+"剩餘車位："+ str(service["AvailableSpaces"]))
@@ -79,7 +38,7 @@ async def serviceArea():
 async def cityParking(cityName):
     url = "https://tdx.transportdata.tw/api/basic/v1/Parking/OffStreet/ParkingAvailability/City/"+cityName+"?%24top=30&%24format=JSON"
     cityParkingSpace = []
-    dataAll = response(url)
+    dataAll = get_data_response(app_id, app_key, url)
     for city in dataAll["ParkingAvailabilities"]:
         cityParkingSpace.append(city["CarParkName"]["Zh_tw"]+"剩餘車位："+ str(city["AvailableSpaces"]))
     return {"cityParkingSpace":cityParkingSpace}
@@ -88,7 +47,7 @@ async def cityParking(cityName):
 async def sideParking(cityName):
     url = "https://tdx.transportdata.tw/api/basic/v1/Parking/OnStreet/ParkingSegmentAvailability/City/"+cityName+"?%24top=30&%24format=JSON"
     sideParkingSpace = []
-    dataAll = response(url)
+    dataAll = get_data_response(app_id, app_key, url)
     for side in dataAll["CurbParkingSegmentAvailabilities"]:
         sideParkingSpace.append(side["ParkingSegmentName"]["Zh_tw"] +" 剩餘位置： " +str(side["AvailableSpaces"]))
     return {"sideParking":sideParkingSpace}
